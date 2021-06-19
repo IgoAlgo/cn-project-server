e46ff957860f8b8b653313ffe6b71bb5638b8976
Jin <gimquokka@gmail.com>
2021-06-19 21:28:18 +0900

    init

---
+++ src/main/java/gist/cnprojectserver/CnProjectServerApplication.java
@@ -0,0 +1,13 @@
+package gist.cnprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnProjectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gist/cnprojectserver/CnProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gist.cnprojectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnProjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

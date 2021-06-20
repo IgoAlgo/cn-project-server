c52994320b06c522eb5ad7688221fb282d511954
Jin <gimquokka@gmail.com>
2021-06-20 23:15:17 +0900

    submission version

--- src/main/java/gist/cnprojectserver/controller/ElbController.java
+++ src/main/java/gist/cnprojectserver/controller/ElbController.java
@@ -7,21 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ElbController {
-
+    /*
     @Value("${ami-id:N/A}")
     private String amiId;
-
+    @Value("${instance-type:N/A}")
+    private String instanceType;
+    @Value("${services/domain:N/A}")
+    private String serviceDomain;
+    */
     @Value("${hostname:N/A}")
     private String hostname;
 
-    @Value("${instance-type:N/A}")
-    private String instanceType;
-
-    @Value("${services/domain:N/A}")
-    private String serviceDomain;
-
     @GetMapping("/index")
     public ResponseEntity<String> getInstanceInfo() {
-        return ResponseEntity.ok().body(amiId + "\n\r\n\r" + hostname + "\n\r\n\r" + instanceType + "\n\r" + serviceDomain);
+        return ResponseEntity.ok().body(hostname);
     }
 }
